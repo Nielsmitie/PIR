3d260b2a0adb6767dca3d13aa913c0ff169927a1
Nielsmitie <niels.nuthmann@googlemail.com>
2019-01-15 17:02:56 +0100

    startet

--- project/recommendation/item_similarity.py
+++ project/recommendation/item_similarity.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
 
-class RecommondationEngine:
+
+class RecommendationEngine:
+
+    path = '../data/hackathon_02_hetrec2011-delicious-2k/'
 
     def __init__(self):
         self.bookmark_infos = pd.read_csv(self.path + 'bookmarks.dat',
@@ -9,16 +12,17 @@ class RecommondationEngine:
                                           index_col=['id'],
                                           usecols=['id', 'title'],
                                           encoding='ISO-8859-15')
-
+        # load website and tags
         self.word_df = self._get_words_by_id()
-
-
+        # load user and preferences
+        
+        print('test')
 
     def _get_words_by_id(self):
         url_tags = pd.read_csv(self.path + 'bookmark_tags.dat',
                                sep='\t',
                                index_col=['bookmarkID'],
-                               usecols=['bookmarkID', 'tagID'])
+                               usecols=['bookmarkID', 'tagID', 'tagWeight'])
         url_tags.columns = ['id']
         id_to_tag = pd.read_csv(self.path + 'tags.dat',
                                 sep='\t',
@@ -32,3 +36,7 @@ class RecommondationEngine:
             return [i[0] for i in x.values.tolist()]
 
         return id_words.groupby(level=0).apply(test)
+
+
+if __name__ == '__main__':
+    RecommendationEngine()
