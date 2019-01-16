eed50ada463c4415b84f525808e38fe57c64819c
Nielsmitie <niels.nuthmann@googlemail.com>
2019-01-16 13:49:42 +0100

    Added all files, added url in the result, fixed bug that displays unimportant results

--- project/embeddings/glove_embeddings.py
+++ project/embeddings/glove_embeddings.py
@@ -10,14 +10,14 @@ from project.embeddings.tag_embeddings import TagEmbedding
 
 class GloveEmbedding(TagEmbedding):
 
-    def __init__(self):
+    def __init__(self, glove_file):
         self.bookmark_infos = pd.read_csv(self.path + 'bookmarks.dat',
                                           sep='\t',
                                           index_col=['id'],
-                                          usecols=['id', 'title'],
+                                          usecols=['id', 'title', 'urlPrincipal'],
                                           encoding='ISO-8859-15')
         # super(GloveEmbedding, self).__init__()
-        glove_file = '../data/glove.6B/glove.6B.100d.txt'
+
         tmp_file = get_tmpfile('test_word2vec.txt')
 
         glove2word2vec(glove_file, tmp_file)
@@ -25,8 +25,12 @@ class GloveEmbedding(TagEmbedding):
         self.model = KeyedVectors.load_word2vec_format(tmp_file, binary=False)
 
         self.word_df = self._get_words_by_id()
+        self.bookmark_infos = self.bookmark_infos.join(self.word_df)
         self.word_lists = self.word_df.values.tolist()
 
 
 if __name__ == '__main__':
-    print(GloveEmbedding().search('firefox'))
+    glove_file = ['../data/glove.6B/glove.6B.100d.txt', '../data/glove.42B.300d.txt', '../data/glove.840B.300d.txt',
+                  '../data/glove.6B/glove.6B.50d.txt', '../data/glove.6B/glove.6B.200d.txt',
+                  '../data/glove.6B/glove.6B.500d.txt']
+    print(GloveEmbedding(glove_file[0]).search('firefox addons extensions'.split(' ')))
