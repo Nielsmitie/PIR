75e5e9bf1cc0afe6987ab7e87bd1dd482df4c408
Nielsmitie <niels.nuthmann@googlemail.com>
2019-01-22 16:27:35 +0100

    only use most similar findings

--- project/page_rank/page_rank.py
+++ project/page_rank/page_rank.py
@@ -5,20 +5,20 @@ from project.embeddings.tag_embeddings import TagEmbedding
 
 class PageRank:
 
-    path = '../data/hackathon_02_hetrec2011-delicious-2k/'
+    path = 'data/hackathon_02_hetrec2011-delicious-2k/'
 
     def __init__(self):
         self.url_tags = pd.read_csv(self.path + 'bookmark_tags.dat',
                                     sep='\t',
                                     index_col=['bookmarkID'],
                                     usecols=['bookmarkID', 'tagWeight'])
+        self.url_tags = self.url_tags.groupby('bookmarkID').sum()
 
     def page_rank(self, similarity_ranking, top_k=20):
-        self.url_tags = self.url_tags.groupby('bookmarkID').sum()
         test = self.url_tags.join(similarity_ranking, on=['bookmarkID'])
 
         sorted = test.sort_values(by=['similarity', 'tagWeight'], ascending=False, na_position='last')
-        sorted = sorted.iloc[:100].sort_values(by=['tagWeight', 'similarity'], ascending=False)
+        sorted = sorted.loc[sorted['similarity'] > 0.7].sort_values(by=['tagWeight', 'similarity'], ascending=False)
         return sorted.iloc[:top_k]
 
 
